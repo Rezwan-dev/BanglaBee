fe8e62491e55a2a5904a4c09575e7a7092daa95e
rezwan <rezwan.dev@gmail.com>
2017-02-27 15:18:17 +0600

    work work work

--- app/src/main/java/com/codestation/banglabee/SingleWord.java
+++ app/src/main/java/com/codestation/banglabee/SingleWord.java
@@ -38,6 +38,7 @@ public class SingleWord extends DialogFragment {
                              Bundle savedInstanceState) {
         convertView = inflater.inflate(R.layout.card, container, false);
         convertView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        init();
         return convertView;
     }
     
@@ -56,23 +57,7 @@ public class SingleWord extends DialogFragment {
 
         TextView textViewCard5 = (TextView) convertView.findViewById(R.id.en_syn);
         textViewCard5.setText(wordModel.getEnglishDefination());
-        convertView.findViewById(R.id.playBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String audioFileName = wordModel.getAudioFileName();
-                Log.e("taggy", audioFileName);
-                audioFileName = audioFileName.replace(".mp3", "");
-//        audioFileName = audioFileName.replace(".", "");
-                int resId = getResourceId(audioFileName, "raw", getActivity().getPackageName());
-                if( resId > 0) {
-                    MediaPlayer mPlayer = MediaPlayer.create(getContext(), resId);
-                    mPlayer.start();
-                }
-                else{
-                    Log.e("taggy", "PlayAudio : media file not found: " + audioFileName + " resId: " + resId);
-                }
-            }
-        });
+        convertView.findViewById(R.id.playBtn).setVisibility(View.GONE);
     }
 
     private int getResourceId(String pVariableName, String pResourcename, String pPackageName) {
--- database/src/main/java/com/codestation/banglabee/mashroor/databasemanagement/DatabaseHelper.java
+++ database/src/main/java/com/codestation/banglabee/mashroor/databasemanagement/DatabaseHelper.java
@@ -34,7 +34,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      */
     public DatabaseHelper(Context context) {
 
-        super(context, DB_NAME, null, 1);
+        super(context, DB_NAME, null, 2);
         this.myContext = context;
         DB_PATH= myContext.getDatabasePath(DB_NAME).toString();
     }
